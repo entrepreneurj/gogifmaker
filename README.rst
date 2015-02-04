bac01dc084a73d4d2fda786732184e704b6e0bb2
J Letts <entrepreneurj@gmail.com>
2015-02-03 23:20:31 -0500

    Version 1.0 - fully working-ish

--- README.rst
+++ README.rst
@@ -2,3 +2,14 @@ gifmaker
 ========
 
 Simple command line tool that turns all the images in a directory into a gif.
+
+Made with lots of help from https://www.socketloop.com/tutorials/
+
+TODO
+====
+
+* Clean up code - A lot!
+* Allow user input options for other directories and the delay time between frames
+* Allow user to choose gif output name
+* Optimse for speed
+* If images are different sizes, make the size of the final gif equal to the size of the largest image, and plot everything from the center.
--- gifmaker.go
+++ gifmaker.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
-	//"image/gif"
-	//_ "image/png"
-	//_ "image/jpeg"
-	//"image"
+	"image/gif"
+	_ "image/png"
+	_ "image/jpeg"
+	"image/draw"
+	"image/color/palette"
+	"image"
 	"os"
 	"path/filepath"
-	
+	"log"	
 )
 
 // Global var holding redis connection
@@ -30,7 +32,7 @@ func main() {
 		os.Exit(1)
 	}
 	defer d.Close()
-
+	
 	files, err := d.Readdir(-1)
 	if err != nil {
 		fmt.Println(err)
@@ -38,14 +40,40 @@ func main() {
 	}
 
 	fmt.Println("Reading " + dirname)
+	
+	var my_gif gif.GIF
+	my_gif.LoopCount =1
+
+	var opt gif.Options
+	opt.NumColors = 256
 
 	for _, file := range files {
 		if file.Mode().IsRegular() {
 			if filepath.Ext(file.Name()) == ".png" || filepath.Ext(file.Name()) == ".jpeg" || filepath.Ext(file.Name()) == ".jpg" {
 				fmt.Println(file.Name(), file.Size(), "bytes")
+				reader, err := os.Open(file.Name())
+				m, _, err := image.Decode(reader)
+				new_img := image.NewPaletted(m.Bounds(), palette.Plan9)
+				draw.Draw(new_img, m.Bounds(), m, image.ZP, draw.Src)
+				my_gif.Image = append(my_gif.Image, new_img)
+				my_gif.Delay = append(my_gif.Delay, 1)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 		}
 
 	}
+
+	out, err := os.Create("./finalGif.gif")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	err = gif.EncodeAll(out, &my_gif)
+	if err != nil {
+		log.Fatal(err)
+	}
+	out.Close()
 }
 
